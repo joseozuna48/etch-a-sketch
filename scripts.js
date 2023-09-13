bbd163ba39fd252900340f4ff9100e85ca5d4d7d
Jose Ozuna <joseozuna48@gmail.com>
2023-09-13 13:29:00 -0700

    Add slider that modifies grid size

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const sketch_section = document.querySelector(".sketch-grid");
+let slider = document.querySelector(".slider");
+let gridSize = document.querySelector(".gridSize");
 
 
 function createGrid(num) {
-
+    
     let size =  Math.floor(100/num); 
     sketch_section.style.gridTemplateColumns = `repeat(${num},1fr)`;
 
@@ -21,4 +23,24 @@ function colorBackground(){
     this.style.backgroundColor  = "black";
 }
 
-createGrid(50);
+function deleteGrid (){
+    while(sketch_section.firstChild){
+        sketch_section.removeChild(sketch_section.firstChild);
+    }
+}
+
+
+
+slider.addEventListener("mouseup",()=>{
+    
+    deleteGrid();
+    createGrid(slider.value);
+    
+});
+
+slider.addEventListener("input",()=>{
+    gridSize.textContent = `${slider.value} X ${slider.value} `;
+})
+
+
+createGrid(16);
